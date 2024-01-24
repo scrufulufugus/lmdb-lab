bbe533a3645be32215cf51cb67d412a171e36b10
Will Alicar <will@alicar.me>
2024-01-23 16:29:28 -0800

    add working failing test for SlottedPage

--- tests/test_heap_storage.cpp
+++ tests/test_heap_storage.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "heap_storage.h"
 #include "storage_engine.h"
+#include <iostream>
 
 namespace
 {
@@ -9,21 +10,49 @@ namespace
         EXPECT_EQ(1, 1);
     }
 
-    TEST(make_slotted_page, does_init_work)
+    // Slotted Page
+    TEST(slotted_page, add_data)
     {
-        std::string text = "Hello";
-        void *data = (void *)(&text);
-        Dbt dbt(data, (u_int32_t)(sizeof(data)));
-        SlottedPage(dbt, 1);
+        char *new_bytes = new char[DbBlock::BLOCK_SZ];
+        Dbt *data = new Dbt(new_bytes, DbBlock::BLOCK_SZ);
+        RecordID id = 0;
+        SlottedPage *page = new SlottedPage(*data, id, true); // set this to true so page->add wouldn't seg fault
+
+        // data marshalling
+        std::string text = "hello";
+        uint size = text.length();
+        uint offset = 0;
+
+        char *bytes = new char[DbBlock::BLOCK_SZ];
+        *(u_int16_t*) (bytes + offset) = size;
+        offset += sizeof(u_int16_t);
+        memcpy(bytes+offset, text.c_str(), size);
+        offset += size;
+
+        char *right_size_bytes = new char[offset];
+        memcpy(right_size_bytes, bytes, offset);
+        delete [] bytes;
+        Dbt *text_data = new Dbt(right_size_bytes, offset);
+        //
+
+        RecordID text_id = page->add(text_data);
+
+        // need to unmarshal this!
+        char* result = (char *)page->get(text_id);
+        delete page;
+        delete new_bytes;
+        ASSERT_EQ(text.c_str(), result);
     }
 
-    TEST(make_heap_file, does_init_work)
+    // Heap File
+    TEST(heap_file, does_init_work)
     {
         std::string text = "my_heapfile";
         HeapFile file(text);
     }
 
-    TEST(make_heap_table, does_init_work)
+    // Heap Table
+    TEST(heap_table, does_init_work)
     {
         Identifier identifier = "my_identifier";
         ColumnNames column_names{"a", "b"};
